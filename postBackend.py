267e520b4e91f5e637555f90da27f091a6812bf8
KennaReed <mreed16@calpoly.edu>
2021-02-03 16:57:32 -0800

    working on connecting Atlas to backend

--- postBackend.py
+++ postBackend.py
@@ -1,1 +1,15 @@
-Testing 
+from flask import Flask
+from flask_cors import CORS
+import db
+
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route('/')
+def flask_mongodb_atlas():
+    return 'flask mongodb atlas!'
+
+@app.route("/test")
+def test():
+   return "Connected to the data base!" 
