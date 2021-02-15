00bf958b7987cac5ecf1eb09c31caf3f98b4da99
KennaReed <mreed16@calpoly.edu>
2021-02-14 19:21:35 -0800

    Sprint 1 - McKenna

--- postBackend.py
+++ postBackend.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
-import db
+from flask import request
+from flask import jsonify
+from model_mongodb import Post
 
 app = Flask(__name__)
 
@@ -10,6 +12,17 @@ CORS(app)
 def flask_mongodb_atlas():
     return 'flask mongodb atlas!'
 
-@app.route("/test")
-def test():
-   return "Connected to the data base!" 
+@app.route('/posts', methods=['GET', 'POST'])
+def get_posts():
+    if request.method == 'GET':
+        posts = Post().find_all()
+        return {"posts_list": posts}
+    elif request.method == 'POST':
+        postToAdd = request.get_json()
+        newPost = Post(postToAdd)
+        newPost.save()
+        resp = jsonify(newPost), 201
+        return resp
+
+if __name__ == '__main__':
+    app.run(port=5000)
