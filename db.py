da219f16c88ca4e180449c45927a50a05c600e17
KennaReed <34385914+KennaReed@users.noreply.github.com>
2021-02-14 19:22:50 -0800

    Delete db.py
    Combined it with model mongo file

--- db.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_pymongo import pymongo
-
-CONNECTION_STRING = "mongodb+srv://mReed:gnqKIQpbxhDGrfeq@csc-308-teamd.crp9s.mongodb.net/GetInvolved\-\CalPoly?retryWrites=true&w=majority"
-
-client = pymongo.MongoClient(CONNECTION_STRING)
-db = client.get_database('flask_mongodb_atlas')
-user_collection = pymongo.collection.Collection(db, 'user_collection')
