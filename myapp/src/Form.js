d9709f2a35540b4778e1a8843e64c8606692c6ab
billc2501 <billc2501@gmail.com>
2021-03-30 18:40:29 -0700

    UI design and bug fix

--- myapp/src/Form.js
+++ myapp/src/Form.js
@@ -72,8 +72,8 @@ function Form(props) {
      <div >
         <form>
  
-           <label htmlFor="title">Name of event:</label>
-           <input 
+           <label class="event" htmlFor="title">Name of event:</label>
+           <input class="event1"
              type="text"  
              id="title" 
              name="title"
@@ -82,8 +82,8 @@ function Form(props) {
              </input>
            <br></br>
 
-           <label htmlFor="DatePosted">Today's date:</label>
-           <input 
+           <label class="date" htmlFor="DatePosted">Today's date:</label>
+           <input class="date1"
              type="date" 
              id="DatePosted" 
              name="DatePosted"
@@ -92,8 +92,8 @@ function Form(props) {
              </input>
            <br></br>
  
-           <label htmlFor="DateEvent">Date of event:</label>
-           <input 
+           <label class="eventd" htmlFor="DateEvent">Date of event:</label>
+           <input class="eventd1"
              type="date" 
              id="DateEvent" 
              name="DateEvent"
@@ -102,8 +102,8 @@ function Form(props) {
              </input>
            <br></br>
            
-           <label htmlFor="time">Time of event:</label>
-           <input 
+           <label class="eventt" htmlFor="time">Time of event:</label>
+           <input class="eventt1"
              type="time" 
              id="time" 
              name="time"
@@ -112,8 +112,8 @@ function Form(props) {
              </input>
            <br></br>
  
-           <label htmlFor="publisher">Poster:</label>
-           <input 
+           <label class="poster" htmlFor="publisher">Poster:</label>
+           <input class="poster1"
              type="text" 
              id="publisher"
              name="publisher"
@@ -122,8 +122,8 @@ function Form(props) {
               </input>
            <br></br>
 
-           <label htmlFor="Location">Location of event:</label>
-           <input 
+           <label class="location" htmlFor="Location">Location of event:</label>
+           <input class="location1"
              type="text" 
              id="Location"
              name="Location"
@@ -132,8 +132,8 @@ function Form(props) {
               </input>
            <br></br>
  
-           <label htmlFor="content">Description of event:</label>
-           <textarea 
+           <label class="description" htmlFor="content">Description of event:</label>
+           <textarea class="description1"
              id="content" 
              name="content" 
              rows="5" cols="50"
@@ -142,18 +142,18 @@ function Form(props) {
              </textarea>
            <br></br>
            
-           <label htmlFor="keyWords">What category is the event under:</label>
-             <select name="keyWords" id="keyWords" value={post.keyWords} onChange = {handleChange}>
+           <label class="category" htmlFor="keyWords">What category is the event under:</label>
+             <select class="category1" name="keyWords" id="keyWords" value={post.keyWords} onChange = {handleChange}>
              <option value="Select">Select</option>
              <option value="Sports">Sports</option>
-             <option value="Muisc">Music</option>
+             <option value="Music">Music</option>
              <option value="Community">Community</option>
              <option value="Art">Art</option>
              </select>
              <br></br>
 
-             <label htmlFor="Cost">About how much does the event cost:</label>
-             <select name="Cost" id="Cost" value={post.Cost} onChange = {handleChange}>
+             <label class="cost" htmlFor="Cost">About how much does the event cost:</label>
+             <select class="cost1" name="Cost" id="Cost" value={post.Cost} onChange = {handleChange}>
              <option value="SelectC">Select</option>
              <option value="Free">Free</option>
              <option value="Five">About 5</option>
@@ -162,13 +162,13 @@ function Form(props) {
              </select>
              <br></br>
 
-            <label htmlFor="image">Insert your own image:</label>
+            <label class="image" htmlFor="image">Insert your own image:</label>
       
-            <input type="file" id = "image" name = "image" value = {post.image} onChange={handleChange}/>
+            <input class="image1" type="file" id = "image" name = "image" value = {post.image} onChange={handleChange}/>
          
              <br></br>
 
-           <input type="submit" value="Submit" onClick={submitForm}></input>
+           <input class="submit" type="submit" value="Submit" onClick={submitForm}></input>
          </form>
        
          </div>
--- myapp/src/Home.js
+++ myapp/src/Home.js
@@ -2,15 +2,13 @@ import React from "react";
 
 function Home(){
     return(
-        <div>
-            <h1>
-            Events and Recent Posts : 
-        </h1>	
-        <p>
-        Example Post 1:
-        Meet up at the Pac! Freshman Welcome! Come meet other students!
-        Time: 12:30 PM  
-        </p>
+        <div class="home">
+            <p class="bio">
+            Welcome to our site! Below you can find multiple posts. 
+            By clicking on calendar, you can check out all the events during a certain period of time. 
+            The forum page is where you can make comments on events or ask questions. 
+        </p>	
+        
         </div>
     )
 }
