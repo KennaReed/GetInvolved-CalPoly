e9ae7021cbbbbcd9a2407792bb3c155cc6b6068b
KennaReed <mreed16@calpoly.edu>
2021-04-21 15:26:06 -0700

    Nav Bar UI

--- myapp/src/better.js
+++ myapp/src/better.js
@@ -12,19 +12,10 @@ const Header = () => {
   return (
     <div className="header">
       <div className="logo-nav">
-        <div className="logo-container">
-          <h1>GetInvolved - Cal Poly</h1>
-          <a href="/post">
-            <Logo class="logo" />
-          </a>
-        </div>
-
+        <h1>GetInvolved - Cal Poly </h1>
         <ul>
           <li>
             <Link to="/home">HOME</Link>
-          </li>
-          <li>
-            <Link to="/post">POST</Link>
           </li>
           <li>
             <Link to="/forum">FORUM</Link>
@@ -32,11 +23,20 @@ const Header = () => {
           <li>
             <Link to="/calendar">CALENDAR</Link>
           </li>
+          <li>
+            <Link to="/post">POST</Link>
+          </li>
         </ul>
+
+        <div className="logo-container">
+          <a href="/post">
+            <Logo class="logo"/>
+          </a>
+        </div>
       </div>
       <ul className="signin-up">
         <li className="sign-in">
-          <a href="#">SIGN-IN</a>
+          <a href="#" className="signup-btn">SIGN-IN</a>
         </li>
         <li>
           <a href="" className="signup-btn">
