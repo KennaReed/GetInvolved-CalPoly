97ea15fde088b0031302ce876c544d101d82dd9b
KennaReed <mreed16@calpoly.edu>
2021-04-30 15:59:02 -0700

    some improvement

--- src/Home.js
+++ src/Home.js
@@ -13,20 +13,20 @@ function formatDate(datestr) {
     pad(dateobj.getMonth()+1)+'/'+dateobj.getFullYear();
 }
 
-function DisplayPost(post, index) {
+function DisplayPost(props) {
   return (
-    <div key={index} className={styles.whole}>
+    <div className={styles.whole}>
       <div className={styles.shiftText}>
         <div className={styles.top}>
-          {console.log(post.title)}
-          <p className={styles.title}> {post.title} </p>
+          {console.log(props.post.title)}
+          <p className={styles.title}> {props.post.title} </p>
           <p className={styles.datePosted}>
-            Posted On: {formatDate(post.DatePosted)} </p>
+            Posted On: {formatDate(props.post.DatePosted)} </p>
         </div>
 
-        <p className={styles.description}> {post.content} </p>
-        {handleEvent(post)}
-        <p className={styles.generalInfo}>Cost: {post.Cost}</p>
+        <p className={styles.description}> {props.post.content} </p>
+        {handleEvent(props.post)}
+        <p className={styles.generalInfo}>Cost: {props.post.Cost}</p>
       </div>
     </div>
   );
