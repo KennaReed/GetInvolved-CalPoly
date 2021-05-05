9e830db76839f8312334faca6015816d1c17bed2
dathash <alexanderhartford@gmail.com>
2021-05-05 15:12:26 -0700

    Resolved backend calendar. Still some kinks.

--- src/Cal.js
+++ src/Cal.js
@@ -1,39 +1,59 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Calendar, momentLocalizer} from 'react-big-calendar';
 import moment from 'moment';
+import axios from 'axios';
 
 import './App.css';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const localizer = momentLocalizer(moment);
 
-class Cal extends Component {
-    state = {
-      events: [
-        {
-          start: moment().toDate(),
-          end: moment()
-              .add(1, 'days')
-              .toDate(),
-          title: 'Some title',
-        },
+function Cal() {
+  const [events, setEvents] = useState([]);
+  useEffect(() => {
+    fetchAll().then((result) => {
+      if (result) {
+        setEvents(result);
+      }
+    });
+  }, []);
 
-      ],
-    };
-
-    render() {
-      return (
-        <div className="App">
-          <Calendar
-            localizer={localizer}
-            defaultDate={new Date()}
-            defaultView="month"
-            events={this.state.events}
-            style={{height: '100vh'}}
-          />
-        </div>
-      );
+    async function fetchAll() {
+      try {
+        const response = await axios.get('https://getinvolvedapi.herokuapp.com/forum');
+        let eventsList = []
+        let postsList = response.data.posts_list;
+        for (let i = 0; i < postsList.length; i++) {
+          if (postsList[i].DateEvent !== "") {
+            let time = moment.utc(postsList[i].DateEvent).toDate()
+            let event = {
+              start: time,
+              end: moment(time),
+              title: postsList[i].title,
+            }
+            eventsList.push(event)
+          }
+        }
+      return eventsList;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
     }
+
+
+
+    return (
+      <div className="App">
+        <Calendar
+          localizer={localizer}
+          defaultDate={new Date()}
+          defaultView="month"
+          events={events}
+          style={{height: '100vh'}}
+        />
+      </div>
+    );
 }
 
 export default Cal;
