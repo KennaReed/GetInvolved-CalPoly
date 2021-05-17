6dc01067672bf9a2216d3dff666bbd65554d644e
KennaReed <mreed16@calpoly.edu>
2021-05-17 10:25:59 -0700

    drop down comments and posting box added not functional

--- src/ForumPosts.js
+++ src/ForumPosts.js
@@ -2,9 +2,11 @@ import React, {useState} from 'react';
 import styles from './forum.module.css';
 import {FaAngleDown} from 'react-icons/fa';
 import moment from 'moment';
+import { Button } from 'react-bootstrap';
 
 function ForumPost(props) {
   const [open, setOpen] = useState(false);
+  const [comments, setComments] = useState(false);
 
   function getDetails() {
     if (open) {
@@ -14,9 +16,45 @@ function ForumPost(props) {
           <p>Cost: {props.postData.Cost}</p>
           {handleEvents()}
           {handleLocation()}
+          {getComments()}
+          <Button onClick={() => showComments()}>Show Comments</Button>
         </div>
       );
     }
+  }
+
+  function getComments() {
+    console.log("in getComments");
+    if (comments) {
+      return (
+        <div>
+          {postCommentBox()}
+          <p>Comments!</p>
+        </div>
+      );
+    }
+  }
+
+  function showComments() {
+    console.log("In showComments");
+    setComments(!comments);
+    console.log(comments);
+  }
+
+  function postCommentBox() {
+    return (
+      <div>
+        <label htmlFor="postingComment">Enter Comment</label>
+            <input 
+              type="text"
+              id="postingComment"
+              name="postingComment"
+              // value={post.title}
+              // onChange={handleChange}
+              >
+            </input>
+      </div>
+    );
   }
 
   function handleLocation() {
@@ -38,8 +76,8 @@ function ForumPost(props) {
 
   function formatDate(datestr) {
     const dateobj = new Date(datestr);
-    return pad(dateobj.getDate())+'/'+
-      pad(dateobj.getMonth()+1)+'/'+dateobj.getFullYear();
+    return pad(dateobj.getMonth()+1)+'/'+
+      pad(dateobj.getDate())+'/'+dateobj.getFullYear();
   }
 
   function handleEvents() {
